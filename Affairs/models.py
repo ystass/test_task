7ed8eca57437a93f735771fd19359655daac964c
ystass <stepanischevyy@mail.ru>
2025-02-06 18:56:12 +0400

    создание модели Tasks

--- Affairs/models.py
+++ Affairs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"null": True, "blank": True}
+
+
+class Tasks(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Название задачи")
+    description = models.TextField(verbose_name="Описание задачи")
+    due_date = models.DateTimeField(verbose_name="Срок исполнения", **NULLABLE)
+    tags = models.CharField(max_length=50, verbose_name="Теги", **NULLABLE)
+    is_completed = models.BooleanField(default=False, verbose_name="Задача выполнена?")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
