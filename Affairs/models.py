4d3ae7c2a58d394bb4ee0bfe79d591e066a92abb
ystass <stepanischevyy@mail.ru>
2025-02-07 20:26:54 +0400

    редактирование модели Tasks

--- Affairs/models.py
+++ Affairs/models.py
@@ -4,11 +4,22 @@ NULLABLE = {"null": True, "blank": True}
 
 
 class Tasks(models.Model):
+    TAGS = (
+        ("Urgent", "Срочная"),
+        ("On control", "На контроле"),
+        ("Waiting", "В ожидании"),
+        ("For discussion", "На обсуждение"),
+        ("In progress", "В процессе"),
+    )
+    EXECUTION = (
+        (True, "Выполнена"),
+        (False, "Не выполнена"),
+    )
     title = models.CharField(max_length=50, verbose_name="Название задачи")
     description = models.TextField(verbose_name="Описание задачи")
     due_date = models.DateTimeField(verbose_name="Срок исполнения", **NULLABLE)
-    tags = models.CharField(max_length=50, verbose_name="Теги", **NULLABLE)
-    is_completed = models.BooleanField(default=False, verbose_name="Задача выполнена?")
+    tags = models.CharField(max_length=50, choices=TAGS, verbose_name="Теги", **NULLABLE)
+    is_completed = models.BooleanField(default=False, choices=EXECUTION, verbose_name="Задача выполнена?")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
 
     def __str__(self):
